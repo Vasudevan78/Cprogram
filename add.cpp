938b8283e6bd141b03816ca03a2b00ec75241274
Vasudevan78 <ivasu78@gmail.com>
2017-06-16 14:00:59 +0530

    Update and rename .gitignore to add.cpp

---
+++ add.cpp
@@ -0,0 +1,20 @@
+#include<iostream.h>
+#include<conio.h>
+class add
+{
+ int a, b;
+ public:
+ add()
+ {
+  a=10; b=20;
+ }
+ void display()
+ {
+  cout<<a+b;
+ }
+ }
+ void main()
+ {
+  add a1;
+  a1.display();
+  }
