da709e47ef187ea9b9342105f550886202592e75
Vasudevan78 <ivasu78@gmail.com>
2017-07-11 17:54:56 +0530

    alphabet78.c

---
+++ alphabet88.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+   char s1;
+    scanf("%c",&s1);
+    if( (s1>='a' && s1<='z') || (s1>='A' && s1<='Z'))
+        printf("%c",s1);
+    else
+        printf("%c",s1);
+    return 0;
+}
