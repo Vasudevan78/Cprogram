0989f59c3d6250735f2d5f4f79fa628d5b0a7117
Vasudevan78 <ivasu78@gmail.com>
2017-07-11 18:02:57 +0530

    Create biggest.c

---
+++ biggest.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+int a,b,c;
+printf("enter the number");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b&&a>c)
+prinft("%d",a);
+else if(b>c)
+printf("%d",b);
+else
+printf("%d",c);
+}
